bcaa80aee23ecb5afec3e2f58dc62d1d024bba35
dbistuer <dbistuer@msn.com>
2022-03-19 20:54:52 +0100

    list cinemas

--- Projecte/settings.py
+++ Projecte/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-vz!ba3q!^=8y0ud!(i4a#eo8qib0-x6&l#8&e7q-pk%skk^9iv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
