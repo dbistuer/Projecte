e557b37252440c551a7dbcb8175879ed14c50993
dbistuer <dbistuer@msn.com>
2022-03-19 03:42:13 +0100

    change db name and list movies

--- Projecte/settings.py
+++ Projecte/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'Projecte.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'Teatre.sqlite3',
     }
 }
 
