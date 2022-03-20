d4714d1494e9b0b0021304f1d18d2739f89523d9
dbistuer <dbistuer@msn.com>
2022-03-20 15:09:31 +0100

    collect

--- Projecte/settings/base.py
+++ Projecte/settings/base.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-#import django_heroku
 from pathlib import Path
 import os
 
@@ -33,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Teatre'
+    'Teatre.apps.TeatreConfig'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +103,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR
+STATICFILES_DIR = (os.path.join('static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
