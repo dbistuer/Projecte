5d99706f705ad80b9817ccc52db1077fdbe44bd2
dbistuer <dbistuer@msn.com>
2022-03-20 14:12:44 +0100

    hjgv

--- Projecte/settings/prod.py
+++ Projecte/settings/prod.py
@@ -18,7 +18,7 @@ DATABASES = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://cinemawebproject.herokuapp.com',
                  'https://cinemawebproject.herokuapp.com/']
