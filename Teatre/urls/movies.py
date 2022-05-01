94c1781d9bc9fa5e1b7046c6fb655ba5a34b4307
dbistuer <dbistuer@msn.com>
2022-05-01 17:01:23 +0200

    add movie doing modify modified movie model adding jquery to base file

--- Teatre/urls/movies.py
+++ Teatre/urls/movies.py
@@ -2,8 +2,10 @@ from django.urls import path
 from Teatre import views
 
 urlpatterns_movies = [
-    path('Movies/List', views.MovieList, name='list_movies'),
-    path('Movies/Detail', views.MovieDetail, name='movie_detail'),
-    path('Movies/Movie/<int:id>/<str:type>', views.Movie_, name='movie'),
-    path('Movies/Movie/<int:id>', views.Movie_, name='movie_detail_'),
+    path('Movies/List', views.Movie_List, name='list_movies'),
+    path('Movies/Movie/<int:id>/<str:type>', views.Movie_, name='movie_edit_delete'),
+    path('Movies/Movie/Edit/<int:id>', views.Movie_Edit, name='movie_edit'),
+    path('Movies/Movie/Delete/<int:id>', views.Movie_, name='movie_delete'),
+    path('Movies/Movie/<int:id>', views.Movie_, name='movie_detail'),
+    path('Movies/New_Movie',views.New_Movie,name='new_movie')
 ]
