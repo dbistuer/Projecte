cacedeb27c4dcd2036f5e8d2feeeab61a4c2bf2b
Marc Vivas <vivasmarc001@gmail.com>
2022-05-01 11:12:02 +0200

    Added comments in views/account.py

--- Teatre/views/account.py
+++ Teatre/views/account.py
@@ -29,6 +29,9 @@ return render(request, 'hola.html', {'request': request})
 
 """
 
+"""
+This view shows on screen information of a logged user 
+"""
 @login_required
 def profile(request):
     user = request.user
@@ -36,13 +39,22 @@ def profile(request):
     json = {'user': client, }
     return render(request, 'User/profile.html', json)
 
+"""
+In this view you should be able to modify information of a logged user
+Client modification
+"""
 @login_required
 def edit_profile(request):
+    # Get the logged client
     user = request.user
     client = Client.objects.get(user=user)
+
+    # Show the form
     if request.method == 'GET':
         json = {'user': client, }
         return render(request, 'User/ModifyProfile.html', json)
+
+    # Apply modifications
     elif request.method == 'POST':
         # Get form data
         name = request.POST['name']
@@ -59,6 +71,7 @@ def edit_profile(request):
             json = {'error': error, 'register': False}
             return render(request, 'registration/InvalidValues.html', json)
 
+        # Apply changes
         if alias:
             # Check if user exists
             if User.objects.filter(username=alias).exists() and not user.username == alias:
@@ -84,11 +97,19 @@ def edit_profile(request):
         client.save()
         return redirect('profile')
 
+"""
+Creation of a new client
+With this view you should be able to create a new client.
+The default role of a new client is normal_user 
+"""
 def SignIn(request):
+    # Show the form
     if request.method == 'GET':
         return render(request, 'registration/SignIn.html')
 
+    # Create the client
     if request.method == 'POST':
+        # Get form data
         name = request.POST['name']
         DNI = request.POST['DNI']
         address = request.POST['address']
@@ -108,10 +129,12 @@ def SignIn(request):
             json = {'error': error, 'register': True}
             return render(request, 'registration/InvalidValues.html', json)
 
+        # Check if user already exists
         if User.objects.filter(username=alias).exists():
             json = {'error': 'Username already exist, you will have to choose another one.', 'register': True}
             return render(request, 'registration/InvalidValues.html', json)
 
+        # Create the client
         user = User(username=alias, email=email, first_name=name)
         user.set_password(password)
         user.save()
@@ -124,10 +147,16 @@ def SignIn(request):
         client.save()
         return redirect('login')
 
+"""
+Client elimination
+A logged client should be able to delete his own account
+"""
 @login_required
 def delete_user(request):
+    # Show form
     if request.method == 'GET':
         return render(request, 'User/delete_user.html', {'user': request.user})
+    # Proceed to delete the client
     elif request.method == 'POST':
         user = request.user
         json = {'name': user.username,}
@@ -136,9 +165,14 @@ def delete_user(request):
         user.delete()
         return render(request, 'User/user_deleted.html', json)
 
+"""
+Only the superuser/admin can have access to this view.
+This view provides to an admin user the tools to delete and change roles of the rest of the clients.
+"""
 @login_required
 @user_passes_test(lambda user: user.is_superuser)
 def user_management(request):
+    # Show a list of all the users
     if request.method == 'GET':
         staff_users = User.objects.filter(is_staff=True, is_superuser=False, is_active=True)
         normal_users = User.objects.filter(is_active=True, is_staff=False, is_superuser=False)
@@ -147,13 +181,16 @@ def user_management(request):
                                                              'normal_clients': normal_users,
                                                              'admin_clients': admin_users,
                                                              'user': request.user,})
+    # Delete OR change roles of a single user.
     elif request.method == 'POST':
+        # Delete client
         if 'delete' in request.POST.keys():
             user = User.objects.get(username=request.POST['delete'])
             client = Client.objects.get(user=user)
             client.delete()
             user.delete()
 
+        # Change client role
         elif 'user_role' in request.POST.keys():
             user = User.objects.get(username=request.POST['username'])
             if request.POST['user_role'] == 'admin':
@@ -174,7 +211,9 @@ def user_management(request):
         return redirect('user_management')
 
 
-
+"""
+Given a DNI, cardNumber and a phoneNumber, this function validates if the given values are in a correct format
+"""
 def validate_data(DNI='', cardNumber='', phoneNumber=''):
     error = ''
     if DNI:
