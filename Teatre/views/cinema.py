e3aa3d799038f825806d36758ff5406cff1aefdf
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-02 17:45:18 +0200

    Added CinemaDetail

--- Teatre/views/cinema.py
+++ Teatre/views/cinema.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from Teatre.models import *
 
+def CinemaDetail(request):
+    cinemas = Cinema.objects.all()
+    json = {'cinemas': cinemas}
+    return render(request, 'Cinema/Detail.html', json)
 
 def CinemaList(request):
     cinemas = Cinema.objects.all()
