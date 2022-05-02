a8aee9307a8d0bf26ae25ede04fb91a7ccaa18a5
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-02 17:49:40 +0200

    added /Detail url

--- Teatre/urls/cinemas.py
+++ Teatre/urls/cinemas.py
@@ -3,4 +3,5 @@ from Teatre import views
 
 urlpatterns_cinemas = [
     path('Cinema/List', views.CinemaList, name='list_cinemas'),
+    path('Cinema/Detail', views.CinemaDetail, name='cinema_detail'),
 ]
