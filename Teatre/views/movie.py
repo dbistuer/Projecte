17a642c09720c1a110d484c4934a219efdb52e70
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-02 10:33:50 +0200

    Update movie.py

--- Teatre/views/movie.py
+++ Teatre/views/movie.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from Teatre.models import *
 
+def MovieDetail(request):
+    movies = Movie.objects.all()
+    json = {'movies':movies}
+    return render(request, 'Movie/Detail.html', json)
+
+def MovieList(request):
+    movies = Movie.objects.all()
+    json = {'movies': movies}
+    return render(request, 'Movie/List.html', json)
