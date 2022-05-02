51742e3e6f1a6412cf50bffc1636f875411f21a2
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-02 10:39:34 +0200

    Update cinema.py
    arreglada funció incorrecta

--- Teatre/views/cinema.py
+++ Teatre/views/cinema.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from Teatre.models import *
 
 
-def ticket_list(request):
-    tickets = Ticket.objects.all()
-    json = {'tickets': tickets}
-    return render(request, 'Ticket/List.html', json)
+def CinemaList(request):
+    cinemas = Cinema.objects.all()
+    json = {'cinemas': cinemas}
+    return render(request, 'Cinema/List.html', json)
