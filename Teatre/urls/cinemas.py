7c368f2fe1b1cf53a791b308ce2d23c56ccdbbdb
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-03 17:25:54 +0200

    update cinemas.py

--- Teatre/urls/cinemas.py
+++ Teatre/urls/cinemas.py
@@ -2,6 +2,10 @@ from django.urls import path
 from Teatre import views
 
 urlpatterns_cinemas = [
-    path('Cinema/List', views.CinemaList, name='list_cinemas'),
-    path('Cinema/Detail', views.CinemaDetail, name='cinema_detail'),
+    path('Cinemas/List', views.Cinema_List, name='list_cinemas'),
+    path('Cinemas/Cinema/<int:id>/<str:type>', views.Cinema_, name='cinema_edit_delete'),
+    path('Cinemas/Cinema/Edit/<int:id>', views.Cinema_Edit, name='cinema_edit'),
+    path('Cinemas/Cinema/Delete/<int:id>', views.Cinema_, name='cinema_delete'),
+    path('Cinemas/Cinema/<int:id>', views.Cinema_, name='cinema_detail'),
+    path('Cinemas/New_Cinema',views.New_Cinema,name='new_cinema')
 ]
