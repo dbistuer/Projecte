15bb1ebcb213cbf2d8d63a096b177aec3bf49390
AdriaRojas <opolo999@hotmail.es>
2022-05-04 09:55:15 +0200

    creacion de sala

--- Teatre/urls/cinemas.py
+++ Teatre/urls/cinemas.py
@@ -3,4 +3,5 @@ from Teatre import views
 
 urlpatterns_cinemas = [
     path('Cinema/List', views.CinemaList, name='list_cinemas'),
+    path('Cinema/new_room',views.Create_sala , name='create_room'),
 ]
--- Teatre/views/cinema.py
+++ Teatre/views/cinema.py
@@ -6,3 +6,21 @@ def CinemaList(request):
     cinemas = Cinema.objects.all()
     json = {'cinemas': cinemas}
     return render(request, 'Cinema/List.html', json)
+
+
+def Create_sala(request):
+    cines_list = list()
+    for cin in Cinema.objects.all():
+        cines_list.append(cin)
+    if request.method == 'POST':
+        capacity = request.POST['capacity']
+        number = request.POST['number']
+        if str.isdigit(request.POST['cinema']):
+            cine = Cinema.objects.get(pk=request.POST['cinema'])
+
+        else:
+            return render(request, '#',{'success' : False, 'cines' : cines_list})
+        Room.objects.create(capacity=capacity,number=number,Cinema_id=cine)
+        return render(request, '#',{'success': True, 'cines': cines_list})
+    else:
+        return render(request, '#',{'cines': cines_list})
