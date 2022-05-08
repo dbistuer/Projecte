2f9a4ba5275b2322c87e07e32578f10717e38bb5
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-08 13:09:50 +0200

    Fixed url discrepancy

--- Teatre/urls/cinemas.py
+++ Teatre/urls/cinemas.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Teatre import views
 
 urlpatterns_cinemas = [
-    path('Cinemas/List', views.Cinema_List, name='list_cinemas'),
+    path('Cinema/List', views.Cinema_List, name='list_cinemas'),
     path('Cinemas/Cinema/<int:id>/<str:type>', views.Cinema_, name='cinema_edit_delete'),
     path('Cinemas/Cinema/Edit/<int:id>', views.Cinema_Edit, name='cinema_edit'),
     path('Cinemas/Cinema/Delete/<int:id>', views.Cinema_, name='cinema_delete'),
