a4182d47cfc4c2ab215047efda2763f534a12718
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-08 12:40:02 +0200

    Fixed function name on home.py

--- Teatre/views/home.py
+++ Teatre/views/home.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from Teatre.models import *
 
 
-def ticket_list(request):
+def home(request):
     tickets = Ticket.objects.all()
     json = {'tickets': tickets}
-    return render(request, 'Ticket/List.html', json)
+    return render(request, 'home.html', json)
