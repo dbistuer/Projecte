1d2d5c671773c19a1b4cb84bf66195eb951adaf7
AdriaRojas <opolo999@hotmail.es>
2022-05-09 18:55:33 +0200

    Modificar y eliminar hecho

--- Teatre/views/cinema.py
+++ Teatre/views/cinema.py
@@ -20,17 +20,33 @@ def Create_sala(request):
             cine = Cinema.objects.get(pk=request.POST['cinema'])
 
         else:
-            return render(request, 'Cinema/new_room.html',{'success': False, 'cines' : cines_list})
-        Room.objects.create(capacity=capacity,number=number,Cinema_id=cine.id)
-        return render(request, 'Cinema/new_room.html',{'success': True, 'cines': cines_list})
+            return render(request, 'Cinema/new_room.html', {'success': False, 'cines': cines_list})
+        Room.objects.create(capacity=capacity, number=number, Cinema_id=cine.id)
+        return render(request, 'Cinema/new_room.html', {'success': True, 'cines': cines_list})
     else:
-        return render(request, 'Cinema/new_room.html',{'success': '.', 'cines': cines_list})
+        return render(request, 'Cinema/new_room.html', {'success': '.', 'cines': cines_list})
 
 
 def room_list(request, id_cinema):
     if request.method == 'GET':
-        #Falta poner la pantalla de errores como dijo el profesor virgili para que no puedan mapear la bbdd (try, except)
-        cine = Cinema.objects.get(id=id_cinema )
+        # Falta poner la pantalla de errores como dijo el profesor virgili para que no puedan mapear la bbdd (try, except)
+        cine = Cinema.objects.get(id=id_cinema)
         rooms = Room.objects.filter(Cinema_id=cine.id)
-        return render(request,'Cinema/room_list.html',{'rooms': rooms, 'cinema' : cine})
+        return render(request, 'Cinema/room_list.html', {'rooms': rooms, 'cinema': cine})
 
+
+def modify_room(request, id_room):
+    if request.method == 'GET':
+        rooms = Room.objects.filter(id=id_room)
+        # falta añadir el id del cineam para saber donde hago el return
+        return render(request, 'Cinema/modify_room.html', {'rooms': rooms})
+    if request.method == 'POST':
+        rooms = Room.objects.filter(id=id_room)
+        room = Room.objects.get(id=id_room)
+        if request.POST['Valor'] == 'modify':
+            room.capacity = str(request.POST['capacity'])
+            room.save()
+            return render(request, 'Cinema/modify_room.html', {'rooms': rooms})
+        if request.POST['Valor']== 'eliminar':
+            room.delete()
+            return render(request,'Cinema/List.html')
