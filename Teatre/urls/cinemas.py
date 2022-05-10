b3022c10e9d29c9462acdcb77dfb9b1431778f09
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-10 17:33:47 +0200

    Changed Cinema_ to Cinema_Delete (line 8)

--- Teatre/urls/cinemas.py
+++ Teatre/urls/cinemas.py
@@ -5,7 +5,7 @@ urlpatterns_cinemas = [
     path('Cinemas/List', views.Cinema_List, name='list_cinemas'),
     path('Cinemas/Cinema/<int:id>/<str:type>', views.Cinema_, name='cinema_edit_delete'),
     path('Cinemas/Cinema/Edit/<int:id>', views.Cinema_Edit, name='cinema_edit'),
-    path('Cinemas/Cinema/Delete/<int:id>', views.Cinema_, name='cinema_delete'),
+    path('Cinemas/Cinema/Delete/<int:id>', views.Cinema_Delete, name='cinema_delete'),
     path('Cinemas/Cinema/<int:id>', views.Cinema_, name='cinema_detail'),
     path('Cinemas/New_Cinema',views.New_Cinema,name='new_cinema')
 ]
