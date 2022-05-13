4c03ef57c2dc7b1bf5d81a136e04c8755acd3ca6
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-13 20:12:27 +0200

    Delete

--- Teatre/views/home.py
+++
@@ -1,9 +0,0 @@
-
-from django.shortcuts import render
-from Teatre.models import *
-
-
-def home(request):
-    tickets = Ticket.objects.all()
-    json = {'tickets': tickets}
-    return render(request, 'home.html', json)
