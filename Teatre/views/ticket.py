4ac13242bc0b0c2a95a3a58bc1316c0041f97539
AlexP-alt <79216577+AlexP-alt@users.noreply.github.com>
2022-05-13 20:02:29 +0200

    Delete ticket.py

--- Teatre/views/ticket.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from Teatre.models import *
-
-
-def ticket_list(request):
-    tickets = Ticket.objects.all()
-    json = {'tickets': tickets}
-    return render(request, 'Ticket/List.html', json)
