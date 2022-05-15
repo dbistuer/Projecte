cbaa774f2ad55b096d788b2595975adaa6d8511a
dbistuer <dbistuer@msn.com>
2022-05-15 15:18:27 +0200

    test error

--- Projecte/settings/prod.py
+++ Projecte/settings/prod.py
@@ -18,7 +18,7 @@ DATABASES = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://cinema-web-project.herokuapp.com/',
                  'https://cinema-web-project.herokuapp.com/']
